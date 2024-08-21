3eb15df99e151bc3423920fcdb9a6a51fdbc2d45
Kamil Kula <xkamil580@gmail.com>
2024-08-21 23:43:43 +0200

    Added form validation and error messages

--- login.php
+++ login.php
@@ -9,34 +9,44 @@
     	<section id="menu">
       		<h1>To-Do</h1>
     	</section>
+		<?php
+			$errormsg='';
+        	$con=new mysqli('localhost','root','','ToDo');
+          	if($_SERVER["REQUEST_METHOD"] == "POST"){
+				$login=$_POST['login'];
+           		$pass=$_POST['pass'];
+				if(empty($login) & empty($pass)){
+					$errormsg='Please enter login and password';
+				}elseif(empty($login)){
+					$errormsg='Please enter login';
+				}elseif(empty($pass)){
+					$errormsg='Please enter password';
+				}else{
+					$sql="SELECT Login, Password FROM USERS WHERE Login LIKE '$login';";
+            		$query=mysqli_query($con,$sql);
+					if($row=mysqli_fetch_array($query)){
+						if($row[1]==$pass){
+							header("Location: home.php");
+						}
+						else{
+							$errormsg='Wrong login or password';
+						}
+					}
+					else{
+						$errormsg='Wrong login or password';
+					}
+          		}
+			}
+		?>
     	<section id="mainA">
         	<h2>Sign in</h2>
         	<form id="form" method="POST">
+				<span class="errormsg"><?php echo $errormsg; ?></span><br>
             	<label>Login</label><input type="text" name="login"><br>
             	<label>Password</label><input type="password" name="pass"><br>
             	<input type="submit" value="Sign in"><br>
             	<p>Don't have an account yet? <a href="register.php">Sign Up</a></p>
         	</form>
-        	<?php
-        		$con=new mysqli('localhost','root','','ToDo');
-          		if(isset($_POST['login']) & isset($_POST['pass'])){
-            		$login=$_POST['login'];
-            		$pass=$_POST['pass'];
-          
-            		$sql="SELECT Login, Password FROM USERS WHERE Login LIKE '$login'";
-            		$query=mysqli_query($con,$sql);
-
-					if($row=mysqli_fetch_array($query)){
-						if($row[1]==$pass){
-							echo "<p>Logged in</p>";
-							header("Location: home.php");
-						}
-						else{
-							echo "<p>Wrong Login or password</p>";
-						}
-					}
-          		}
-			?>
     	</section>
     	<section id="footer">
     		<p>Created by Kamil Kula</p>
