86e257cb72a4fd6e96b7b5854403612d3c85b21f
Kamil Kula <xkamil580@gmail.com>
2024-08-22 00:10:02 +0200

    modified HTML structure

--- home.php
+++ home.php
@@ -11,25 +11,39 @@
   <body>
     <section id="menu">
       <h1>To-Do</h1>
-      <!--<//?php
-        $user="Test";
+      <?php
+        $user="Admin1";
         echo "<h4>$user</h4>";
-      ?//> -->
+      ?>
+    </section>
+    <section id="Task">
+      <?php
+      
+      ?>
     </section>
     <section id="addT">
-      <input type="text" placeholder="Enter your task" value="name">
-      <input type="button" value="Add task" onclick="">
+      <h2>Add new tasks</h2>
+      <input type="text" placeholder="Enter your task" value="Name">
+      <input type="button" value="+">
       <br>
-      <input type="date">
-      <select>
+      <select id="tag">
         <option>Work</option>
         <option>School</option>
         <option>Home</option>
-        <option disabled>Add more</option>
+      </select>
+      <input type="date">
+      <select id="priority">
+        <option>Very High priority</option>
+        <option>High priority</option>
+        <option>Medium priority</option>
+        <option>Low priority</option>
       </select>
     </section>
     <section id="tasks">
-
+      <h2>Your tasks</h2><hr>
+      <?php
+      
+      ?>
     </section>
     <section id="footer">
       <p>Created by Kamil Kula</p>
